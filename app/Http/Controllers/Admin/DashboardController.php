e12cca389ca13df7145fd9da8665165bb8d0f529
stevenhuge <ridhokarunia999@gmail.com>
2026-04-15 15:07:37 +0700

    Amikom Hub | Admin

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index() {
+        return view('admin.dashboard');
+    }
+
+    public function indexAdmin() {
+        return view('admin.events');
+    }
+
+    public function transactionsAdmin() {
+        return view('admin.transactions');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\EventController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TicketController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [TicketController::class, 'ticket'])->name('ticket');
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/events', [DashboardController::class, 'indexAdmin'])->name('events.index');
+    Route::get('/transactions', [DashboardController::class, 'transactionsAdmin'])->name('transactions.index');
+});
+
+
+Route::get('/profil', function() {
+    return view('profil');
+});
+
+Route::get('/katalog', function() {
+    return view('katalog');
+});
+
+Route::get('/bantuan', function() {
+    return view('bantuan');
+});
+
