61b8dc1c4aa9adadf1f81949646fe27cfb9b756c
stevenhuge <ridhokarunia999@gmail.com>
2026-04-22 14:39:39 +0700

    Migration & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,100 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 1. Akun Admin Utama
+
+        \App\Models\User::create([
+            'name' => 'Admin Amikom',
+            'email' => 'admin@amikom.ac.id',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
         ]);
+
+        // 2. Insert Kategori Event
+
+        $category = \App\Models\Category::create([
+            'name' => 'Seminar IT',
+            'slug' => 'seminar-it',
+        ]);
+
+        $category2 = \App\Models\Category::firstOrCreate([
+            'name' => 'Entertaiment',
+            'slug' => 'entertaiment',
+        ]);
+
+        $category3 = \App\Models\Category::firstOrCreate([
+            'name' => 'Competition',
+            'slug' => 'competition',
+        ]);
+
+
+        // 3. Insert Sampel Events
+
+        \App\Models\Event::create([
+            'category_id' => $category2->id,
+            'title' => 'Jazz Night 2025',
+            'description' => 'Nikmati malam yang indah dengan alunan musik jazz yang merdu.',
+            'date' => '2026-05-10 19:00:00',
+            'location' => 'Amikom Baru',
+            'price' => 50000,
+            'stock' => 100,
+            'poster_path' => 'posters/event-1.png'
+        ]);
+
+        \App\Models\Event::create([
+            'category_id' => $category->id,
+            'title' => 'Hackaton - Unleash Your Inner Developer',
+            'description' => 'Ayo asah skill coding kamu dan ciptakan solusi inovatif untuk tantangan masa depan',
+            'date' => '2026-05-05 10:00:00',
+            'location' => 'Inkubator Amikom',
+            'price' => 50000,
+            'stock' => 100,
+            'poster_path' => 'posters/event-2.png',
+        ]);
+
+        \App\Models\Event::create([
+            'category_id' => $category->id,
+            'title' => 'AI % FUTURE TECH SUMMIT 2026',
+            'description' => 'Jelajahi tren terkini dalam kecerdasan buatan dan teknologi masa depan bersama para ahli di bidangnya.',
+            'date' => '2026-05-01 13:00:00',
+            'location' => 'Cinema Unit 6',
+            'price' => 50000,
+            'stock' => 100,
+            'poster_path' => 'posters/event-3.png',
+        ]);
+
+        \App\Models\Event::create([
+            'category_id' => $category3->id,
+            'title' => 'GEMASI AMIKOM 2026',
+            'description' => 'Ayo ikuti kompetisi Gelar Karya Mahasiswa Sistem Informasi Universitas Amikom Yogyakarta, Ada banyak kategori yang menarik',
+            'date' => '2026-02-01 12:00:00',
+            'location' => 'Online',
+            'price' => 5000,
+            'stock' => 100,
+            'poster_path' => 'posters/event-4.png',
+        ]);
+
+        \App\Models\Event::create([
+            'category_id' => $category3->id,
+            'title' => 'AMICTA AMIKOM 2026',
+            'description' => 'Buat kamu Mahasiswa Universitas Amikom Yogyakarta, Ayo ikuti kompetisi bergengsi AMICTA! ',
+            'date' => '2026-07-01 08:00:00',
+            'location' => 'Ruang Citra 2',
+            'price' => 10000,
+            'stock' => 100,
+            'poster_path' => 'posters/event-5.png',
+        ]);
+
+        \App\Models\Event::create([
+            'category_id' => $category->id,
+            'title' => 'Seminar Internasional Trading',
+            'description' => 'Ikuti seminar internasional tentang dunia trading yang menarik',
+            'date' => '2026-05-01 13:00:00',
+            'location' => 'Ruang Citra 1',
+            'price' => 25000,
+            'stock' => 100,
+            'poster_path' => 'posters/event-6.png',
+        ]);
+        
     }
 }
