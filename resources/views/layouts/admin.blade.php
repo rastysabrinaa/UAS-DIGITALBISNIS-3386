2d5027b4fbfc481c1acfd9f1981d5bf219e50926
Stevenhuge <ridhokarunia999@gmail.com>
2026-04-29 15:10:47 +0700

    update haeder events

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -96,21 +96,25 @@ py-3 text-indigo-300 hover:text-white transition font-medium text-left">
         </div>
     </aside>
 
-    <!-- Main Content -->
     <main class="flex-1 p-10 overflow-y-auto w-full">
-        <!-- Header -->
-        @if(session('success'))
+    <!-- Header -->
+    <header class="flex justify-between items-center mb-10">
+        <div>
+            <h1 class="text-3xl font-black">@yield('page_title')</h1>
+            <p class="text-slate-500 font-medium">@yield('page_subtitle')</p>
+        </div>
+    </header>
 
-        <div class="bg-green-100 text-green-700 p-4 rounded-xl mb-6
-
-font-bold text-sm">
-
+    @if(session('success'))
+        <div class="bg-green-100 text-green-700 p-4 rounded-xl mb-6 font-bold text-sm">
             {{ session('success') }}
         </div>
-        @endif
+    @endif
 
-        @yield('content')
-    </main>
+    @yield('content')
+</main>
+
+    
 </body>
 
 </html>
